65a5269c69ffef386d489d41f1720cf2c687eeaa
Achmad Muzammi Fahmi <145878328+muzammifahmi@users.noreply.github.com>
2025-01-19 21:26:43 +0700

    Create assignmentoperator.c

---
+++ assignmentoperator.c
@@ -0,0 +1,22 @@
+//assignment operator with C language
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int a=8, b=4, x=8, y=4;
+	printf("\nNilai A   = %d", a);
+	printf("\nNilai ++A = %d", ++a);
+	printf("\nNilai A   = %d", a);
+	printf("\nNilai B   = %d", b);
+	printf("\nNilai --B = %d", --b);
+	printf("\nNilai B   = %d", b);
+	
+	printf("\nNilai X   = %d", x);
+	printf("\nNilai ++X = %d", ++x);
+	printf("\nNilai X   = %d", x);
+	printf("\nNilai Y   = %d", y);
+	printf("\nNilai --Y = %d", --y);
+	printf("\nNilai Y   = %d", y);
+	
+	getch();
+}
