22f44435d04c4e40dae4087650b132eb5e9bb6d7
Achmad Muzammi Fahmi <145878328+muzammifahmi@users.noreply.github.com>
2025-01-19 21:40:17 +0700

    Create logical_statement.cpp

---
+++ logical_statement.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    int p=255;
+    cout<<"[p>=0 && p>=255] ->"<<(p>=0 && p>=255)<<endl;
+    cout<<"[p<=0 || p<=255] ->"<<(p<=0 || p<=255)<<endl;
+    getch();
+}
