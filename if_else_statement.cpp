757ff16ea313257fed315671a6879c9760a4470b
Achmad Muzammi Fahmi <145878328+muzammifahmi@users.noreply.github.com>
2025-01-20 16:53:01 +0700

    Create if_else_statement.cpp

---
+++ if_else_statement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+	cout<<"masukkan nilai : ";
+	cin>>a;
+    if(a<20)
+    {
+        cout<<"a kurang dari 20"<<endl;//will appear if the value is less than 20
+    }
+    else {cout<<"nilai a adalah: "<<a<<endl;}
+    return 0;
+}
